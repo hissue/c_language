93303a4b9f347f445280160bac9f6747d5b8bf58
hissue <68486112+hissue@users.noreply.github.com>
2020-08-11 13:17:35 +0900

    Create malloc.c
    #naver_newbi

---
+++ malloc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //int 자료형 3개로 이루어진 list 라는 포인터를 선언하고 메모리 할당
+    int *list = malloc(3 * sizeof(int));
+
+    // 포인터가 잘 선언되었는지 확인
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    // list 배열의 각 인덱스에 값 저장
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    //int 자료형 4개 크기의 tmp 라는 포인터를 선언하고 메모리 할당
+    int *tmp = malloc(4 * sizeof(int));
+
+    if (tmp == NULL)
+    {
+        return 1;
+    }
+
+    // list의 값을 tmp로 복사
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    // tmp배열의 네 번째 값도 저장
+    tmp[3] = 4;
+
+    // list의 메모리를 초기화
+    free(list);
+
+    // list가 tmp와 같은 곳을 가리키도록 지정
+    list = tmp;
+
+    // 새로운 배열 list의 값 확인
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    // list의 메모리 초기화
+    free(list);
+}
+
+/*단순하게 현재 배열이 저장되어 있는 메모리 위치의 바로 옆에 일정 크기의 메모리를 더 덧붙이면 되겠지만, 
+실제로는 다른 데이터가 저장되어 있을 확률이 높습니다. 따라서 안전하게 새로운 공간에 큰 크기의 메모리를 
+다시 할당하고 기존 배열의 값들을 하나씩 옮겨줘야 합니다.*/
